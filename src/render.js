3882cb82fac0c094ea147beb994f639b12f87e4f
Lingdong Huang <i@lingdong.works>
2019-12-14 14:15:38 -0500

    basic readme

--- src/render.js
+++ src/render.js
@@ -7,7 +7,7 @@ try{
 
 const RED = "#E53"
 const BLACK = "#222"
-
+const FONT = "Source Han Serif TC"
 const BOOK_COLORS = {
   'ctrl':RED,
   'lop':BLACK,
@@ -54,6 +54,9 @@ function render(fname,txt,{plotResult=false}={}){
 
 	var PCT = 40;
 	var PCB = 5;
+
+	var PSH = -2;
+	var MA = 10;
 
 	txt = txt.replace(/\r\n/g,"\n");
 	txt = txt.replace(/\n+/g,"");
@@ -112,12 +115,12 @@ function render(fname,txt,{plotResult=false}={}){
 		}
 
 		for (var i = 0; i < fname.length; i++){
-			svg += `<text x="${(side?0:(W2-X1))+X1*0.1}" y="${vo+30+i*X1*1.5}" font-size="${X1*0.8}" font-family="serif" fill="${BLACK}">${fname[i]}</text>`
+			svg += `<text x="${(side?0:(W2-X1))+X1*0.1}" y="${vo+30+i*X1*1.5}" font-size="${X1*0.8}" font-family="${FONT}" fill="${BLACK}">${fname[i]}</text>`
 
 		}
 		var hpg = num2hanzi(pageno+1);
 		for (var i = 0; i < hpg.length; i++){
-			svg += `<text x="${(side?0:(W2-X1))+X1*0.1}" y="${uo+30+i*X1*1.5}" font-size="${X1*0.8}" font-family="serif" fill="${BLACK}">${hpg[i]}</text>`
+			svg += `<text x="${(side?0:(W2-X1))+X1*0.1}" y="${uo+30+i*X1*1.5}" font-size="${X1*0.8}" font-family="${FONT}" fill="${BLACK}">${hpg[i]}</text>`
 		}
 	}
 
@@ -125,7 +128,10 @@ function render(fname,txt,{plotResult=false}={}){
 	var y;
 
 	function newSvg(){
-		svg = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="${W2}" height="${H}">`;
+		svg = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="${W2}" height="${H}" viewBox="${-MA} ${-MA} ${W2+MA*2} ${H+MA*2}">`;
+		if (MA){
+			svg += `<rect x="${-MA}" y="${-MA}" width="${W2+MA*2}" height="${H+MA*2}" stroke="none" fill="white"></rect>`
+		}
 		svg += `<rect x="${0}" y="${0}" width="${W}" height="${H}" stroke="${BLACK}" fill="linen"></rect>`
 		svg += `<rect x="${W}" y="${0}" width="${W}" height="${H}" stroke="${BLACK}" fill="linen"></rect>`
 
@@ -189,7 +195,7 @@ function render(fname,txt,{plotResult=false}={}){
 				x-=CW/3.7;
 				
 			}else{
-				svg += `<text x="${x}" y="${y}" font-size="${CS/3}" font-family="serif" fill="rgba(0,0,0,0.7)">${out[i]}</text>`
+				svg += `<text x="${x}" y="${y}" font-size="${CS/3}" font-family="${FONT}" fill="rgba(0,0,0,0.7)">${out[i]}</text>`
 				y+=CH/2.5;
 				if (y >= Y1){
 					y=Y0+CH;
@@ -217,7 +223,7 @@ function render(fname,txt,{plotResult=false}={}){
 							if (cmt[k]=="。"){
 								svg += `<circle cx="${kx+CW/2-CO}" cy="${ky-CH/2}" r="${CH/12}" stroke="${RED}" fill="none" stroke-width="0.5"></circle>`
 							}else{
-								svg += `<text x="${kx}" y="${ky}" font-size="${CS/2.2}" font-family="serif" fill="${BOOK_COLORS.comment}">${cmt[k]}</text>`
+								svg += `<text x="${kx}" y="${ky}" font-size="${CS/2.2}" font-family="${FONT}" fill="${BOOK_COLORS.comment}">${cmt[k]}</text>`
 								ky += CH/1.5;
 								if (ky>Y0-PCB){
 									ky = PCT+CH/2;
@@ -231,8 +237,8 @@ function render(fname,txt,{plotResult=false}={}){
 					}
 				}
 			}else if (txt[i] == "。"){
-				// svg += `<text x="${x+CW*0.6+CO}" y="${y-CH}" font-size="${CS}" font-family="serif" fill="${RED}">${txt[i]}</text>`
-				svg += `<circle cx="${x+CW-3}" cy="${y-CH}" r="${CH/7}" stroke="${RED}" fill="none" stroke-width="1" wy-data="${txt[i]}"></circle>`
+				// svg += `<text x="${x+CW*0.6+CO}" y="${y-CH}" font-size="${CS}" font-family="${FONT}" fill="${RED}">${txt[i]}</text>`
+				svg += `<circle cx="${x+CW-3}" cy="${y-CH+PSH}" r="${CH/7}" stroke="${RED}" fill="none" stroke-width="1" wy-data="${txt[i]}"></circle>`
 			}else if (txt[i] == "「"){
 				iden = true;
 				if (y > Y1){
@@ -240,8 +246,8 @@ function render(fname,txt,{plotResult=false}={}){
 					
 				}
 				var h = CH/4
-				svg += `<path d="M${x} ${y-CH/2+h/2} L${x+CW/5} ${y-CH/2-h/2} L${x+4*CW/5} ${y-CH/2-h/2} L${x+CW} ${y-CH/2+h/2}" stroke="${BLACK}" fill="none" stroke-width="0.5" wy-data="${txt[i]}"></path>`
-				// svg += `<text x="${x+CO}" y="${y-CH/2}" font-size="${CS}" font-family="serif">︹</text>`
+				svg += `<path d="M${x} ${y-CH/2+h/2+PSH} L${x+CW/5} ${y-CH/2-h/2+PSH} L${x+4*CW/5} ${y-CH/2-h/2+PSH} L${x+CW} ${y-CH/2+h/2+PSH}" stroke="${BLACK}" fill="none" stroke-width="0.5" wy-data="${txt[i]}"></path>`
+				// svg += `<text x="${x+CO}" y="${y-CH/2}" font-size="${CS}" font-family="${FONT}">︹</text>`
 				y += CH/2;
 			}else if (txt[i] == "」"){
 				iden = false;
@@ -250,14 +256,14 @@ function render(fname,txt,{plotResult=false}={}){
 					
 				}
 				var h = CH/4
-				svg += `<path d="M${x} ${y-CH/2-h} L${x+CW/5} ${y-CH/2} L${x+4*CW/5} ${y-CH/2} L${x+CW} ${y-CH/2-h}" stroke="${BLACK}" fill="none" stroke-width="0.5" wy-data="${txt[i]}"></path>`
-				// svg += `<text x="${x+CO}" y="${y}" font-size="${CS}" font-family="serif">︺</text>`
+				svg += `<path d="M${x} ${y-CH/2-h+PSH} L${x+CW/5} ${y-CH/2+PSH} L${x+4*CW/5} ${y-CH/2+PSH} L${x+CW} ${y-CH/2-h+PSH}" stroke="${BLACK}" fill="none" stroke-width="0.5" wy-data="${txt[i]}"></path>`
+				// svg += `<text x="${x+CO}" y="${y}" font-size="${CS}" font-family="${FONT}">︺</text>`
 				y += CH/2;
 			}else{
 				if (y >= Y1){
 					nextLine();
 				}
-				svg += `<text x="${x+CO}" y="${y}" font-size="${CS}" font-family="serif" fill="${BOOK_COLORS[sm[i]]}" wy-data="${txt[i]}">${txt[i]}</text>`
+				svg += `<text x="${x+CO}" y="${y}" font-size="${CS}" font-family="${FONT}" fill="${BOOK_COLORS[sm[i]]}" wy-data="${txt[i]}">${txt[i]}</text>`
 				y += CH;
 			}
 			
@@ -293,7 +299,7 @@ function test_render(){
 		fs.writeFileSync("../render"+i+".svg",svgs[i])
 	}
 }
-
+test_render()
 
 try{
 	module.exports={render,unrender}
