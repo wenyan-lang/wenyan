9a1fb27a747a88a917376f0ad380301ebd1f024d
Anthony Fu <anthonyfu117@hotmail.com>
2019-12-20 04:30:08 +0800

    test: add snapshot testing for examples test

---
+++ test/examples.test.js
@@ -0,0 +1,72 @@
+var fs = require("fs-extra");
+var parser = require("../src/parser");
+var execSync = require("child_process").execSync;
+var { expect } = require("chai");
+
+const exampleDir = "./examples/";
+const outputDir = "./test/temp/examples/";
+const python = getPythonExecutable();
+
+function getPythonExecutable() {
+  try {
+    const output = execSync(`python3 -V`).toString();
+    if (output && +output[7] === 3) return "python3";
+  } catch (e) {}
+  try {
+    const output = execSync(`python -V`).toString();
+    if (output && +output[7] === 3) return "python";
+  } catch (e) {}
+  return undefined;
+}
+
+function runExample(lang, name) {
+  var txt = fs.readFileSync(exampleDir + name + ".wy").toString();
+  var compiled = parser.compile(lang, txt, { logCallback: () => {} });
+  expect(compiled).to.matchSnapshot();
+  const filename = `${outputDir}${name}.${lang}`;
+  fs.writeFileSync(filename, compiled, "utf-8");
+
+  if (lang == "py") {
+    const output = execSync(`${python} ${filename}`, {
+      encoding: "utf-8"
+    }).toString();
+    expect(output).to.matchSnapshot();
+  } else if (lang == "js") {
+    const output = execSync(`node ${filename}`, {
+      encoding: "utf-8"
+    }).toString();
+    expect(output).to.matchSnapshot();
+  }
+}
+
+function runAll(lang) {
+  var files = fs.readdirSync(exampleDir);
+  for (const file of files) {
+    const filename = file.split(".")[0];
+    it(filename, () => runExample(lang, filename));
+  }
+}
+
+describe("examples", () => {
+  before(() => {
+    fs.removeSync(outputDir);
+    fs.ensureDirSync(outputDir);
+  });
+
+  describe("javascript", () => {
+    runAll("js");
+  });
+
+  /* FIXME: there are errors for python compiler
+  if (python) {
+    describe("python", () => {
+      runAll("py");
+    })
+  }
+  else {
+    describe("python", () => {
+      it("skipped", ()=>{})
+    })
+  }
+  */
+});
--- tools/utils.js
+++ tools/utils.js
@@ -46,13 +46,4 @@ function beautifier() {
   return js_beautify;
 }
 
-function pyeval(py) {
-  fs.writeFileSync("tmp.py", py);
-  var ret = execSync(
-    "which python3; if [ $? == 0 ]; then python3 tmp.py; else python tmp.py; fi; rm tmp.py",
-    { encoding: "utf-8" }
-  );
-  return ret;
-}
-
-module.exports = { uglifier, beautifier, catsrc, remotelib, pyeval };
+module.exports = { uglifier, beautifier, catsrc, remotelib };
