870b0b22517691e423aec4447a5bd833af448209
Anthony Fu <anthonyfu117@hotmail.com>
2019-12-20 13:54:07 +0800

    chore: webpack and npm publish

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+module.exports = {
+  target: 'node',
+  entry: {
+    cli: './src/cli.js',
+    core: './src/parser.js'
+  },
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: '#!/usr/bin/env node',
+      raw: true,
+    }),
+  ],
+};
