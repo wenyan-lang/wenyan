aadd31b489a02fada84238bd96a7df97c35f3b04
jiangzoi <zjiang1@andrew.cmu.edu>
2019-12-24 21:46:48 -0800

    add C++ Lexer

---
+++ antlr/wenyanLexer.h
@@ -0,0 +1,68 @@
+
+// Generated from wenyan.g4 by ANTLR 4.7.2
+
+#pragma once
+
+
+#include "antlr4-runtime.h"
+
+
+
+
+class  wenyanLexer : public antlr4::Lexer {
+public:
+  enum {
+    T__0 = 1, T__1 = 2, T__2 = 3, T__3 = 4, T__4 = 5, T__5 = 6, T__6 = 7, 
+    T__7 = 8, T__8 = 9, T__9 = 10, T__10 = 11, T__11 = 12, T__12 = 13, T__13 = 14, 
+    T__14 = 15, T__15 = 16, T__16 = 17, T__17 = 18, T__18 = 19, T__19 = 20, 
+    T__20 = 21, T__21 = 22, T__22 = 23, T__23 = 24, T__24 = 25, T__25 = 26, 
+    T__26 = 27, T__27 = 28, T__28 = 29, T__29 = 30, T__30 = 31, T__31 = 32, 
+    T__32 = 33, T__33 = 34, T__34 = 35, T__35 = 36, T__36 = 37, T__37 = 38, 
+    T__38 = 39, T__39 = 40, T__40 = 41, T__41 = 42, T__42 = 43, T__43 = 44, 
+    T__44 = 45, STRING_LITERAL = 46, IDENTIFIER = 47, ARITH_BINARY_OP = 48, 
+    LOGIC_BINARY_OP = 49, POST_MOD_MATH_OP = 50, UNARY_OP = 51, PREPOSITION_YU = 52, 
+    PREPOSITION_YI = 53, IF = 54, ELSE = 55, IF_LOGIC_OP = 56, FOR_START_ARR = 57, 
+    FOR_START_ENUM = 58, FOR_START_WHILE = 59, FOR_MID_ARR = 60, FOR_MID_ENUM = 61, 
+    FOR_IF_END = 62, FLOAT_NUM = 63, FLOAT_NUM_KEYWORDS = 64, INT_NUM = 65, 
+    INT_NUM_KEYWORDS = 66, TYPE = 67, BOOL_VALUE = 68, WS = 69, BREAK = 70
+  };
+
+  wenyanLexer(antlr4::CharStream *input);
+  ~wenyanLexer();
+
+  virtual std::string getGrammarFileName() const override;
+  virtual const std::vector<std::string>& getRuleNames() const override;
+
+  virtual const std::vector<std::string>& getChannelNames() const override;
+  virtual const std::vector<std::string>& getModeNames() const override;
+  virtual const std::vector<std::string>& getTokenNames() const override; // deprecated, use vocabulary instead
+  virtual antlr4::dfa::Vocabulary& getVocabulary() const override;
+
+  virtual const std::vector<uint16_t> getSerializedATN() const override;
+  virtual const antlr4::atn::ATN& getATN() const override;
+
+private:
+  static std::vector<antlr4::dfa::DFA> _decisionToDFA;
+  static antlr4::atn::PredictionContextCache _sharedContextCache;
+  static std::vector<std::string> _ruleNames;
+  static std::vector<std::string> _tokenNames;
+  static std::vector<std::string> _channelNames;
+  static std::vector<std::string> _modeNames;
+
+  static std::vector<std::string> _literalNames;
+  static std::vector<std::string> _symbolicNames;
+  static antlr4::dfa::Vocabulary _vocabulary;
+  static antlr4::atn::ATN _atn;
+  static std::vector<uint16_t> _serializedATN;
+
+
+  // Individual action functions triggered by action() above.
+
+  // Individual semantic predicate functions triggered by sempred() above.
+
+  struct Initializer {
+    Initializer();
+  };
+  static Initializer _init;
+};
+
