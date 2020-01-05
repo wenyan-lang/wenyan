cfe87a5dd3f1451bacc66b0af9fa972aca626a80
Anthony Fu <anthonyfu117@hotmail.com>
2020-01-05 21:13:26 +0800

    fix: stdlib testing

--- src/stdlib.js
+++ src/stdlib.js
@@ -3,17 +3,20 @@
 function loadStdlib() {
   let STDLIB = {};
 
-  const raw = require.context("../lib", true, /.*\.wy$/);
+  try {
+    const raw = require.context("../lib", true, /.*\.wy$/);
 
-  raw.keys().forEach(key => {
-    const parts = key.slice(2, -3).split("/");
-    const data = raw(key).default;
-    for (const part of parts.slice(0, -1)) {
-      if (!STDLIB[part]) STDLIB[part] = {};
-      STDLIB = STDLIB[part];
-    }
-    STDLIB[parts[parts.length - 1]] = data;
-  });
+    raw.keys().forEach(key => {
+      const parts = key.slice(2, -3).split("/");
+      const data = raw(key).default;
+      for (const part of parts.slice(0, -1)) {
+        if (!STDLIB[part]) STDLIB[part] = {};
+        STDLIB = STDLIB[part];
+      }
+      STDLIB[parts[parts.length - 1]] = data;
+    });
+  } catch (e) {} // ignore "require.context" error for testing
+
   return STDLIB;
 }
 
