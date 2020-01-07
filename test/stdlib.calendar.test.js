acd4f9d207046e59cbe882bbd33e4203d46e4562
Anthony Fu <anthonyfu117@hotmail.com>
2020-01-07 17:10:53 +0800

    test: use utc as default timezone for test

--- test/stdlib.calendar.test.js
+++ test/stdlib.calendar.test.js
@@ -1,3 +1,5 @@
+require("set-tz")("UTC");
+
 const { createTestUtil } = require("./utils");
 
 const { expectOutput } = createTestUtil({
@@ -10,14 +12,14 @@ describe("stdlib", () => {
     it("言彼之時刻", () => {
       expectOutput(
         "方悟「言彼之時刻」之義。施「言彼之時刻」於四千七百一十四",
-        "巳初一刻三分三十四秒"
+        "丑初一刻三分三十四秒"
       );
     });
 
     it("言彼之日時", () => {
       expectOutput(
         "方悟「言彼之日時」之義。施「言彼之日時」於四千七百一十四",
-        "西元一九六九年己酉年十一月二十四日辛巳日巳初一刻三分三十四秒"
+        "西元一九六九年己酉年十一月二十四日辛巳日丑初一刻三分三十四秒"
       );
     });
   });
